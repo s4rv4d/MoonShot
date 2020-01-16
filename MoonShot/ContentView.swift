6a4d0372ecd21b235a685ceda298524539b97a9d
s4rv4d <sarvadshetty@gmail.com>
2020-01-16 11:12:27 +0530

    scrollView part

--- MoonShot/ContentView.swift
+++ MoonShot/ContentView.swift
@@ -8,9 +8,30 @@
 
 import SwiftUI
 
+struct CustomText: View {
+    var text: String
+    
+    init(_ text: String){
+        print("Creating a custom Text")
+        self.text = text
+    }
+    
+    var body: some View {
+        Text(text)
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ScrollView(.vertical) {
+            VStack(spacing: 10){
+                ForEach(0..<100){
+                    CustomText("\($0)")
+                        .font(.title)
+                }
+            }
+            .frame(maxWidth: .infinity)
+        }
     }
 }
 
