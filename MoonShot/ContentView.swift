1dfc2891b12b8c751ea1cb7272a48f574997ac37
s4rv4d <sarvadshetty@gmail.com>
2020-01-16 11:21:58 +0530

    navigation link

--- MoonShot/ContentView.swift
+++ MoonShot/ContentView.swift
@@ -8,29 +8,15 @@
 
 import SwiftUI
 
-struct CustomText: View {
-    var text: String
-    
-    init(_ text: String){
-        print("Creating a custom Text")
-        self.text = text
-    }
-    
-    var body: some View {
-        Text(text)
-    }
-}
-
 struct ContentView: View {
     var body: some View {
-        ScrollView(.vertical) {
-            VStack(spacing: 10){
-                ForEach(0..<100){
-                    CustomText("\($0)")
-                        .font(.title)
+        NavigationView {
+            List(0..<100) { row in
+                NavigationLink(destination:Text("Detail \(row)")){
+                    Text("row \(row)")
                 }
             }
-            .frame(maxWidth: .infinity)
+            .navigationBarTitle(Text("SwiftUI"))
         }
     }
 }
